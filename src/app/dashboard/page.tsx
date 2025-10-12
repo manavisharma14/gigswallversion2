8f09e347fb02962efd2db759f0a89376eba709f6
manavisharma14 <manavisharma14@gmail.com>
2025-10-12 17:44:22 -0500

    force dynamic dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 // app/dashboard/page.tsx
+export const dynamic = "force-dynamic";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
