49465daa13662e2385611a5fded7f7b318366596
manavisharma14 <manavisharma14@gmail.com>
2025-10-12 21:12:47 -0500

    fixed google provider param

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -52,6 +52,13 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account",   // 👈 forces Google to show the account chooser
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     CredentialsProvider({
       name: "Credentials",
